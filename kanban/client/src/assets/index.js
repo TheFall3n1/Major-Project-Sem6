c32890a5c93a877f3b8826b8ad8b0e8305e42dbd
TheFall3n1 <chaitanya.12015919@lpu.in>
2023-04-19 10:22:45 +0530

    PMS and kanban [both servers not loading]

---
+++ kanban/client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+    images: {
+        logo: require('./images/logo.jpg'),
+        appIcon: require('./images/kanban.svg')
+    },
+    colors: {
+        secondary:'#292929'
+    }
+}
+
+export default assets
